2d3b07e45297f57e999681b872fe43debd809f76
Colleen <colleen.kelly.t@gmail.com>
2014-07-02 11:18:52 -0700

    sin.c: generate sample of approximate length of <timeout>

--- src/sin.c
+++ src/sin.c
@@ -18,21 +18,22 @@ int main(int argc, char ** argv) {
 	int index;
 	double increment;
 	double current;
+	double current_sample;
+	double max_sample;
 	int value;
 
-	alarm(timeout);
-	
 	value = 0;
 	current = 0;
 	increment = 2.0 * M_PI * (double)freeq / sample_rate;
+	max_sample = sample_rate * timeout;
 
-	while (1) {
+	for (current_sample = 0; current_sample < max_sample; current_sample++) {
 		value = (sin(current) + 1) * MAX_INT/2;  // scale to int
 		fprintf(stderr, "%i \n", value);
 		write(1, &value, 4);
 		current += increment;
 		if (current> 2*M_PI) {
-			current-= 2*M_PI;
+			current -= 2*M_PI;
 		}
 	}
 }
